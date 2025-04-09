77c83718ca7057f0ca83f5d8bec0b829f21eb8b6
hdz-kevin <kevinhdez592@gmail.com>
2025-04-09 13:28:50 -0600

    contacts routes with resource route

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,7 @@ use App\Http\Controllers\HomeController;
 Auth::routes();
 
 Route::get('/', fn () => auth()->check() ? redirect()->route('home') : view('welcome'));
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+Route::resource('contacts', ContactController::class);
